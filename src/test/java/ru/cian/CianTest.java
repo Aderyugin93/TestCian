994f6274299ba144960e0e68c633e93e15f07f88
andrejderugin <aderyugin93@gmail.com>
2022-02-28 04:30:08 +0300

    Optimi code

--- src/test/java/ru/cian/CianTest.java
+++ src/test/java/ru/cian/CianTest.java
@@ -1,17 +1,13 @@
 package ru.cian;
-
 import org.junit.Assert;
 import org.junit.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class CianTest {
+public class CianTest extends WebSetting {
+
     @Test
     public void firstTest() {
-        System.setProperty("webdriver.chrome.driver", "/Users/andrejderugin/Downloads/chromedriver");
-        ChromeDriver driver = new ChromeDriver();
         driver.get("https://www.cian.ru/");
         String title = driver.getTitle();
         Assert.assertTrue(title.equals("Циан - база недвижимости в Московской области | Продажа, аренда квартир и другой недвижимости"));
-//        driver.quit();
     }
 }
---
+++ src/test/java/ru/cian/WebSetting.java
@@ -0,0 +1,21 @@
+package ru.cian;
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WebSetting {
+    public ChromeDriver driver;
+
+    @Before
+    public void setting() {
+        System.setProperty("webdriver.chrome.driver", "/Users/andrejderugin/Downloads/chromedriver");
+        driver = new ChromeDriver();
+        System.out.println("Start");
+    }
+
+    @After
+    public void exit() {
+        driver.quit();
+        System.out.println("Exit");
+    }
+}
